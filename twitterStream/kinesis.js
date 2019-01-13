a122391271d6a6de238f1c0c5f0eb03d9734ca33
Darrin Lim <darrin.s.lim@gmail.com>
2019-01-13 14:43:20 -0600

    Finished to-Kinesis Stream

--- twitterStream/kinesis.js
+++ twitterStream/kinesis.js
@@ -16,3 +16,30 @@ var stream = client.stream('statuses/filter', {
   track: 'cat',
   language: 'en'
 });
+
+stream.on('data', function(event) {
+  if (event.text) {
+
+    var record = JSON.stringify({
+      id: event.id,
+      timestamp: event['created_at'],
+      tweet: event.text.replace(/["'}{|]/g, '')
+    }) + '|';
+
+    kinesis.putRecord({
+      Data: record,
+      StreamName: 'twitterStream',
+      PartitionKey: 'key'
+    },
+    function (err, data) {
+      if (err) {
+        console.error(err);
+      }
+      console.log('sending: ', event.text);
+    });
+  }
+});
+
+stream.on('error', function (error) {
+  throw error;
+})
